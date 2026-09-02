b8503004ed7c0fca5cc4a03669a45d7dc2df8e43
juliansiebold <julian.siebold@me.com>
2026-09-02 08:50:27 +0200

    Task State abstraction

--- src/packages/core/models.py
+++ src/packages/core/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from abc import ABC
 from enum import StrEnum
 from uuid import UUID
 
@@ -7,6 +8,7 @@ class TaskStatus(StrEnum):
     RUNNING = 'Running'
     FAILED = 'Failed'
     SUCCEEDED = 'Succeeded'
+
 
 @dataclass(frozen=True)
 class TaskPayload:
@@ -19,15 +21,28 @@ TaskId = UUID
 
 class Lease:
     """Locks a task to an assigned worker. Owns TTL definition"""
-    lease_ttl_seconds: intw
+    lease_ttl_seconds: int
     assigned_worker: WorkerId
     task_id = TaskId
 
 @dataclass
 class Task: 
     '''Representation of an arbitrary task to queue into the system for execution'''
-    task_id = TaskId
-    task_status: TaskStatus = TaskStatus.PENDING
+    task_id: TaskId
+    task_state: "TaskState"
     payload: TaskPayload
 
-    # TODO Needs state managemennt for Task
+class TaskState(ABC):
+    """Abstract interface for Task state handling."""
+
+    def handle_creation(task: Task) -> "TaskState":
+        pass
+
+    def handle_start(task: Task, worker_id: WorkerId) -> "TaskState":
+        pass
+
+    def handle_fail(taks: Task) -> "TaskState":
+        pass
+
+    def handle_success(task: Task) -> "TaskState":
+        pass
