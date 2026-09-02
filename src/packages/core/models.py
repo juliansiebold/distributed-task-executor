9b4b1611c73b8d85c92825e37bd436f99cbd1e27
juliansiebold <julian.siebold@me.com>
2026-09-02 09:14:49 +0200

    #7 abstraction task state & transition_to method of Task

--- src/packages/core/models.py
+++ src/packages/core/models.py
@@ -9,7 +9,6 @@ class TaskStatus(StrEnum):
     FAILED = 'Failed'
     SUCCEEDED = 'Succeeded'
 
-
 @dataclass(frozen=True)
 class TaskPayload:
     """contract for data type."""
@@ -25,24 +24,31 @@ class Lease:
     assigned_worker: WorkerId
     task_id = TaskId
 
+    # TODO evaluate if Lease nneeds to be it's own concet. 
+    # E.g. it could be handled by the service as worker_id makes more sense to be part of Task
+
 @dataclass
 class Task: 
     '''Representation of an arbitrary task to queue into the system for execution'''
     task_id: TaskId
-    task_state: "TaskState"
     payload: TaskPayload
+    _state: "AbstractTaskState"
 
-class TaskState(ABC):
+    def transition_to(self, state: "AbstractTaskState") -> None:
+        """Transitions the task to it's next state"""
+        self._state = state
+
+class AbstractTaskState(ABC):
     """Abstract interface for Task state handling."""
 
-    def handle_creation(task: Task) -> "TaskState":
+    def handle_creation(task: Task) -> None:
         pass
 
-    def handle_start(task: Task, worker_id: WorkerId) -> "TaskState":
+    def handle_start(task: Task, worker_id: WorkerId) -> None:
         pass
 
-    def handle_fail(taks: Task) -> "TaskState":
+    def handle_fail(taks: Task) -> None:
         pass
 
-    def handle_success(task: Task) -> "TaskState":
+    def handle_success(task: Task) -> None:
         pass
